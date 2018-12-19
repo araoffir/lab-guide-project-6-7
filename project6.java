aaab8594255d81124a06e936e69bbb6a19ecb8b6
offir <44310884+araoffir@users.noreply.github.com>
2018-12-20 01:35:06 +0200

    Add files via upload

---
+++ project6.java
@@ -0,0 +1,123 @@
+import java.util.Random;
+
+import javax.swing.plaf.synth.SynthScrollBarUI;
+
+import org.w3c.dom.css.Counter;
+
+public class project6 {
+
+	public static void main(String[] args) {
+		// System.out.println("6*1____________________________________________________");
+		// int a = (int) (Math.random() * 10 + 1);
+		// for (int i = 0; i < a; i++) {
+		// System.out.print(i + ",");
+		// }
+
+		// System.out.print(a);
+		// System.out.println("6*2_______________________________________________________");
+		// int a = (int) (Math.random() * 10 + 1);
+		// int b = (int) (Math.random() * 10 + 1);
+		// System.out.println(a + "," + b);
+		// if (a < b) {while (a <= b) {System.out.print(a + ",");a++;}}
+		// if (b < a) {while (b <= a) {System.out.print(b + ",");b++;}}
+
+		// System.out.println("6*3____________________________________________________________");
+		// int a = (int) (Math.random() * 10 + 1);
+		// System.out.println(a);
+		// for(int i=0;i<a;i++) {if(i%2==0) {System.out.print(i+",");i++;}}
+
+		// System.out.println("6*4______________________________________________________________");
+		// int a = (int) (Math.random() * 100 + 1);
+		// int b = (int) (Math.random() * a - 1);
+		// System.out.println(a + "," + b);
+		// if (b == 0) {
+		// System.out.println("cant devide by 0");
+		// } else {
+		// for (int i = 0; i < a; i++) {
+		// if (i % b == 0) {
+		// System.out.print(i + ",");
+		// }
+		// i++;
+		// }
+		// }
+
+		// System.out.println("6*5________________________________________________________");
+		// int a = (int) (Math.random() * 9999 + 1);
+		// System.out.println(a);
+		// int count = 1;
+		// int sum = 0;
+		// int b = 0;
+		// int c = 0;
+		// while (9 < a) {
+		// b = a % 10;
+		// count++;
+		// sum += b;
+		// c = c * 10;
+		// c = c + a % 10;
+		// a = a / 10;
+		// }
+		// System.out.println("number of digits= " + count);
+		// System.out.println("the left digit is= " + a);
+		// System.out.println("the sum of the digits= " + (sum + a));
+		// System.out.println("the oposite number is= " + c + a);
+
+		// System.out.println("6*6__________________________________________________________________");
+		// int a = (int) (Math.random() * 9999 + 1);
+		// System.out.println("a= "+a);
+		// int r,sum=0;
+		// int b=a;
+		// while (a>0) {
+		// r=a%10;
+		// sum=(sum*10)+r;
+		// a=a/10;
+		// }
+		// if(b==sum)
+		// System.out.println("pal= "+b);
+		// else
+		// System.out.println("no pal");
+
+		// System.out.println("6*7_____________________________");
+		// int a = (int) (Math.random() * 9 + 1);
+		// int i,sum=1;
+		// System.out.println("a= "+a);
+		// for(i=1;i<=a;i++) {
+		// sum=sum*i;
+		// }
+		// System.out.println("sum="+sum);
+
+		// System.out.println("6*8_____________________________________________________");
+		// int a = (int) (Math.random() * 49 + 1);
+		// System.out.println("a= "+a);
+		// int i=1;
+		// int f1=0,f2=1,f3=0;
+		// for (i=1;i<a;i++) {
+		// f3=f1+f2;
+		// f1=f2;
+		// f2=f3;
+		// }
+		// System.out.println("a gave us= "+f3);
+
+		// System.out.println("6*9_______________________________________________");
+		// int a = (int) (Math.random() * 49 + 1);
+		// System.out.println("a= " + a);
+		// int i = 1;
+		// int f1 = 0, f2 = 1, f3 = 0;
+		// for (i = 1; i < a; i++) {
+		// f3 = f1 + f2;
+		// System.out.print(f3+",");
+		// f1 = f2;
+		// f2 = f3;
+		// }
+
+		// System.out.println("6*10__________________________________");
+		// int i = 0;
+		// for (i = 0; i <= 100; i++) {
+		// if (i % 7 == 0 || i % 10 == 7) {
+		// System.out.println("boom");
+		// } else {
+		// System.out.println(i);
+		// }
+		//
+		// }
+	}
+}
---
+++ project7.java
@@ -0,0 +1,82 @@
+import java.sql.Array;
+import java.util.DuplicateFormatFlagsException;
+import java.util.HashSet;
+import java.util.Set;
+
+public class project7 {
+
+	public static void main(String[] args) {
+		// System.out.println("7*1________________________");
+		// int[] arr = new int[10];
+		// double sum = 0;
+		// for(int i=0;i<arr.length;i++) {
+		// arr[i]=(int) (Math.random() *100);
+		// sum+=arr[i];
+		// System.out.print(arr[i]+",");
+		// }
+		// System.out.println("sum= "+sum);
+		// System.out.println("avg= "+(sum/arr.length));
+
+		// System.out.println("7*2_____________");
+		// int[] arr = new int[10];
+		// int max = 0;
+		// for(int i=0;i<arr.length;i++) {
+		// arr[i]=(int) (Math.random() *100);
+		// if(max<arr[i]) {
+		// max=arr[i];
+		// }
+		// System.out.print(arr[i]+",");
+		// }
+		// System.out.println("max number is="+max);
+
+		// System.out.println("7*3___________");
+		// Set<Integer> set = new HashSet<Integer>();
+		// int[] arr = new int[10];
+		// for (int i = 0; i < arr.length; i++) {
+		// arr[i] = (int) (Math.random() * 10);
+		// System.out.print(arr[i] + ",");
+		// }
+		// for (int num : arr) {
+		// set.add(num);
+		// }
+		// System.out.println(set);
+
+		// System.out.println("7*4_____________");
+		// int[] arr = new int[10];
+		//
+		// for(int i=0;i<arr.length;i++) {
+		// (int) (Math.random() *10);
+		// System.out.print(arr[i] + ",");
+		// }
+		// int a=arr[0];
+		// System.out.println();
+		// arr[0]=a;
+		// System.out.print(arr[9]+",");
+		// for(int y=arr.length-1;y>0;y--) {
+		// arr[y]=arr[y-1];
+		// System.out.print(arr[y]+",");
+		// }
+
+		// System.out.println("7*5________________________________");
+		// int[] arr1 = new int[10];
+		// int[] arr2 = new int[20];
+		// int sum=0;
+		// int a=0;
+		// for(int i=0;i<arr2.length;i++) {
+		// arr2[i]=a++;
+		// System.out.print("");
+		//
+		// for(int y=0;y<arr1.length;y++) {
+		// arr1[y] = (int) (Math.random() * 100);
+		// System.out.print(arr1[y]+".");
+		// sum+=arr1[y];
+		// }
+		// System.out.print("(avg= "+sum/arr1.length+" )tests-");
+		// System.out.println("( student "+arr2[i]+")")
+		// }
+		// System.out.println("class "+sum/arr2.length);
+	}
+
+}	
+	
+	
